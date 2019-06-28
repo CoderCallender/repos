9c8d50aa3d2e0a46b3d0c166d34d9b5811978fc1
CoderCallender <42542281+CoderCallender@users.noreply.github.com>
2019-06-28 16:35:22 +0100

    Simple program that starts and stops audio
    Working commit

---
+++ Audio_Player/Audio_Player/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Audio_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.playUpDown = new System.Windows.Forms.NumericUpDown();
+            this.waitUpDown = new System.Windows.Forms.NumericUpDown();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.playUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waitUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Audio Play Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Audio Wait Time";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(69, 199);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "File";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(119, 43);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(354, 20);
+            this.fileTextBox.TabIndex = 4;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(502, 41);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 5;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // playUpDown
+            // 
+            this.playUpDown.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.playUpDown.Location = new System.Drawing.Point(197, 127);
+            this.playUpDown.Name = "playUpDown";
+            this.playUpDown.Size = new System.Drawing.Size(120, 20);
+            this.playUpDown.TabIndex = 6;
+            // 
+            // waitUpDown
+            // 
+            this.waitUpDown.Location = new System.Drawing.Point(197, 161);
+            this.waitUpDown.Name = "waitUpDown";
+            this.waitUpDown.Size = new System.Drawing.Size(120, 20);
+            this.waitUpDown.TabIndex = 7;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(69, 238);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(504, 265);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(79, 13);
+            this.statusLabel.TabIndex = 9;
+            this.statusLabel.Text = "Status: Paused";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(335, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "milliseconds";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 287);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.waitUpDown);
+            this.Controls.Add(this.playUpDown);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.playUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waitUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.NumericUpDown playUpDown;
+        private System.Windows.Forms.NumericUpDown waitUpDown;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Audio_Player/Audio_Player/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Timers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Audio_Player
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player = new SoundPlayer();
+        private static System.Timers.Timer pauseTimer;
+        private static System.Timers.Timer playTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            waitUpDown.Minimum = 1;
+            playUpDown.Minimum = 100;
+
+            waitUpDown.Maximum = 120;
+            playUpDown.Maximum = 1000;
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\Users\Chase\Desktop",
+                Title = "Browse wav Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "wav",
+                Filter = "wav files (*.wav)|*.wav",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileTextBox.Text = openFileDialog1.FileName;
+            }
+
+            else
+            {
+                MessageBox.Show("uh - oh!");
+                return;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                // Assign the selected file's path to 
+                // the SoundPlayer object.  
+                player.SoundLocation = fileTextBox.Text;
+
+                // Load the .wav file.
+                player.Load();
+            }
+            catch (Exception ex)
+            {
+                // ReportStatus(ex.Message);
+                MessageBox.Show("File load error");
+                return;
+            }
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+            waitUpDown.Enabled = false;
+            playUpDown.Enabled = false;
+
+            //load timer for play length
+            playTimer = new System.Timers.Timer();
+            playTimer.Interval = Convert.ToInt32(playUpDown.Value);  
+            playTimer.Elapsed += playEndEvent;
+            playTimer.AutoReset = false;
+
+            //load timer for pause length
+            pauseTimer = new System.Timers.Timer();
+            pauseTimer.Interval = Convert.ToInt32(waitUpDown.Value) * 1000; //time is given in milliseconds
+            pauseTimer.Elapsed += pauseEndEvent;
+            pauseTimer.AutoReset = false;
+            pauseTimer.Enabled = false;
+
+            // Start the timer
+            playTimer.Enabled = true;
+
+            statusLabel.Text = "Status: Playing";
+            player.PlayLooping();
+        }
+
+        //time has come to pause the audio 
+        private void playEndEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The play event was raised at {0}", e.SignalTime);
+            //stop audio
+            player.Stop();
+            //start pause length timer
+            pauseTimer.Enabled = true;
+            playTimer.Enabled = false;
+        }
+
+        //time has come to play the audio 
+        private void pauseEndEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("The pause event was raised at {0}", e.SignalTime);
+            //play audio
+            player.PlayLooping();
+            //start play length timer
+            pauseTimer.Enabled = false;
+            playTimer.Enabled = true;
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            pauseTimer.Enabled = false;
+            playTimer.Enabled = false;
+            player.Stop();
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+            waitUpDown.Enabled = true;
+            playUpDown.Enabled = true;
+            statusLabel.Text = "Status: Paused";
+        }
+    }
+}
